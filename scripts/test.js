3037ba4c1a962aff5faee101900373dc328a3639
bohdanvakh <bohdanvakh@gmail.com>
2026-06-21 19:20:13 +0300

    feat(callbacks) practise callbacks examples

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,54 @@
-let num = 10;
+// Callbacks practise
 
-function inc(callback) {
-    setTimeout(() => {
-        callback()}, 0);
+// 3 task
+function repeat(number, callback) {
+    for (let i = 0; i < number; i++) {
+        callback(i);
+    }
 }
 
-inc(() => {
-    num = 20;
-});
+repeat(10, function(i) {
+    console.log(`Number: ${i}`);
+})
 
-console.log(num);
+// 2 task
+function greet(name, callback, message = "Привіт") {
+    const greetings =  `${message}, ${name}!`;
+    callback(greetings);
+}
+
+greet("Bohdan", function(greetings) {
+    console.log(greetings)}, "Ласкаво просимо"
+);
+
+// 1 task
+function math(a, b, operation) {
+    return operation(a, b);   
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(math(20, 10, add));
+
+// 4 task
+function myMap(arr, callback) {
+    const newArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(callback(arr[i], i, arr));
+    }
+
+    return newArray;
+}
+
+const myArray = [65, 78, 33, 1001];
+
+myMap(myArray, (value, index) => {
+    console.log(`Index: ${index} for value: ${value}`);
+})
