8c88bbdd9703e7d09a199151df645662a86b37db
bohdanvakh <bohdanvakh@gmail.com>
2026-07-24 22:31:00 +0300

    add type ID

--- scripts/intersection.ts
+++ scripts/intersection.ts
@@ -28,5 +28,6 @@ function printCoord(pt: Point) {
 printCoord({ x: 100, y: 200 });
 
 // You can actually use a type alias to give a name to any type at all,
-// not just an object type. For example, a type alias can name a union type:
+// not just an object type. For example, a type alias can name a union type.
 
+type ID = string | number;
